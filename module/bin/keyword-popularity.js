d09bf72bfb492cff9e89f03e6beed8dee2b90bf2
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-08-04 14:57:40 +0200

    Draft implementation (WIP)

--- module/bin/keyword-popularity.js
+++ module/bin/keyword-popularity.js
@@ -3,7 +3,7 @@
 const {stdout, stderr, exit, argv} = process;
 
 const flags = require('minimist')(argv.slice(2), {boolean: true});
-const files = flags._;
+const keywords = flags._;
 
 // Print usage
 
@@ -19,9 +19,45 @@ if (flags.help) stdout.write([
 
 if (flags.h || flags.help) exit(0);
 
-if (!files.length) {  // TODO: Remove if not expecting files
+if (!keywords.length) {
   stderr.write(require('./help/usage'));
   exit(1);
 }
 
-// TODO
+// The logic
+
+const {Promise} = require('es6-promise');
+const npmKeyword = require('npm-keyword');
+const zip = require('trine/iterable/zip');
+const map = require('trine/iterable/map');
+const to = require('trine/iterable/to');
+const easyTable = require('easy-table');
+
+Promise.all(keywords.map(
+  (keyword) => new Promise(
+    (resolve, reject) => npmKeyword(keyword, (error, packages) => {
+      if (error) reject(error);
+      else resolve(packages.length);
+    })
+  )
+))
+  .then((results) => {
+    const popularityTable = easyTable([
+      {1: 'KEYWORD', 2: 'POPULARITY'},
+    ].concat(keywords
+      ::zip(results)
+      ::map(function() {
+        return {1: this[0], 2: this[1]};
+      })
+      ::to(Array)
+    )).print();
+
+    stdout.write(popularityTable + '\n');
+
+    exit(0);
+  })
+  .catch((error) => {
+    stderr.write(error + '\n');
+    exit(1);
+  })
+;
