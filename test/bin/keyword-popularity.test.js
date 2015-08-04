cd7142888fbcebfff2fc3c27e4739599ad526b3f
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-08-04 14:30:25 +0200

    Update bin tests

--- test/bin/keyword-popularity.test.js
+++ test/bin/keyword-popularity.test.js
@@ -7,7 +7,9 @@ const plus = require('1-liners/plus');
 const spawn = require('tape-spawn');
 
 const title = curry(plus)('The CLI tool:  ');
-const keywordPopularity = resolve(__dirname, '../../module/bin/keyword-popularity.js');
+const keywordPopularity = resolve(__dirname,
+  '../../module/bin/keyword-popularity.js'
+);
 const $keywordPopularity = curry(execFile)(keywordPopularity);
 
 tape(title('Prints usage'), (is) => {
@@ -58,15 +60,49 @@ tape(title('Prints usage'), (is) => {
   });
 });
 
-const cwd = resolve(__dirname, '../mock-cwd');
-
 tape(title('Does what it says'), (is) => {
-  const run = spawn(is, `"${keywordPopularity}"`, {cwd});
+  const keywords = [
+    'cli',
+    'tool',
+    `very-very-unlikely-keyword-${Date.now()}`,
+  ];
+  const tabLength = keywords[2].length + 2;
+  const veryUnlikelyKeyword = keywords[2];
+  const program = spawn(is, `'${keywordPopularity}' ${keywords.join(' ')}`);
 
-  run.succeeds(
+  const wordWithTab = (word) => `${word} {${tabLength - word.length}}`;
+
+  program.succeeds(
     'succeeds'
   );
 
-  run.timeout(500);
-  run.end();
+  program.stdout.match(
+    new RegExp(
+      '^' +
+      wordWithTab('KEYWORD') +
+      'POPULARITY' +
+      '\\n'
+    ),
+    'outputs a properly-formatted header…'
+  );
+
+  program.stdout.match(
+    new RegExp(
+      '^.*\\n' +
+      keywords.map((keyword) => (
+        wordWithTab(keyword) +
+        '\\d+\\n'
+      )) +
+      '$'
+    ),
+    '…followed by a table of keywords and numbers in the specified order'
+  );
+
+  program.stdout.match(
+    new RegExp(`${wordWithTab(veryUnlikelyKeyword)}0\\n`),
+    'prints out `0` for a very unlikely keyword'
+  );
+
+  program.timeout(500);
+  program.end();
 });
