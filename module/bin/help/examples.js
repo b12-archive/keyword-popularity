3dc9e71315eaebb91770406903765abed0f484b0
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-08-04 13:42:59 +0200

    Update usage help

--- module/bin/help/examples.js
+++ module/bin/help/examples.js
@@ -3,5 +3,9 @@ const {bold} = require('chalk');
 module.exports =
 `  ${bold('EXAMPLES')}
 
-    …
+    $ keyword-popularity  cli  tool  cli-tool
+    ${bold('KEYWORD')}    ${bold('POPULARITY')}
+    cli        2422
+    tool       497
+    cli-tool   0
 `;
